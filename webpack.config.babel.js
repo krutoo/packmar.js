a4790e11ea4cf4daac969a71cfa39eb0d44a1c06
Dmitry Petrov <dima.krutoo@yandex.ru>
2018-07-22 23:01:27 +0500

    Update configuration

---
+++ webpack.config.babel.js
@@ -0,0 +1,68 @@
+import path from 'path';
+import webpack from 'webpack';
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+
+const getConfiguration = (env, options) => {
+	const isProduction = Boolean(options.mode === 'production');
+	const configuration = {
+		entry: {
+			compo: './src/library/js/index.js',
+			demo: [
+				'./src/demo/js/index.js',
+				'./src/demo/scss/index.scss',
+			],
+		},
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: 'js/[name].js',
+			libraryTarget: 'umd',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+				},
+				{
+					test: /\.scss$/,
+					loader: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: 'css-loader!sass-loader',
+					}),
+				},
+			],
+		},
+		resolve: {
+			alias: {
+				'compo': path.join(__dirname, '/src/library/js/index.js'),
+			},
+		},
+		devtool: 'source-map',
+		optimization: {
+			minimize: isProduction,
+			minimizer: [
+				new UglifyJsPlugin({
+					cache: true,
+					parallel: true,
+					sourceMap: true,
+				})
+			],
+		},
+		plugins: [
+			new ExtractTextPlugin('css/[name].css', {
+				allChunks: true,
+			}),
+		],
+	};
+	if (isProduction) {
+		configuration.plugins.push(
+			new OptimizeCssAssetsPlugin(),
+		);
+	}
+	return configuration;
+};
+
+export default getConfiguration;
