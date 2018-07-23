dd17d05af22741164641001454fccd72bc454ecf
Dmitry Petrov <dima.krutoo@yandex.ru>
2018-07-23 22:48:28 +0500

    update demo

--- src/demo/js/index.js
+++ src/demo/js/index.js
@@ -1,47 +1,19 @@
-import Compo, { html } from 'compo';
+import { compo, render } from 'compo';
 
-function renderField ({ type = 'text', value, id, labelText, input }) {
-	return html`
+function Field ({ type = 'text', value, id, labelText, onInput }) {
+	return compo`
 		<div class="field-wrapper">
 			<label for="${id}">${labelText}</label>
-			<input type="${type}" id="${id}" value="${value}" input="${input}">
+			<input type="${type}" id="${id}" value="${value}" oninput="${onInput}" />
 		</div>
 	`;
 }
 
-class LoginForm extends Compo {
-	render ({ title }) {
-		const username = this.data.username || '';
-		return html`
-			<form class="login-form">
-				<h2 class="heading">${title}</h2>
-				${username.length > 16
-					? html`<div class="warning"><b>Warning: </b>Username is too long</div>`
-					: ''
-				}
-				${renderField({
-					id: 'login-username',
-					value: username,
-					labelText: 'username',
-					input: ({ target }) => this.update({ username: target.value }),
-				})}
-				${renderField({
-					id: 'login-password',
-					type: 'password',
-					value: this.data.password,
-					labelText: 'password',
-					input: ({ target }) => this.update({ password: target.value }),
-				})}
-				<button
-					type="button"
-					click="${() => this.update({ username: '', password: '' })}"
-					class="field-wrapper"
-					disabled="${Boolean(username.length > 16)}"
-				>Submit</button>
-			</form>
-		`;
-	}
-}
+const field = Field({
+	id: 'name-field',
+	value: '',
+	labelText: 'Your name',
+	onInput: ({ target }) => console.log(target.value),
+});
 
-const loginForm = new LoginForm({ title: 'Login' });
-document.body.append(loginForm.compile());
+render(field, document.body);
