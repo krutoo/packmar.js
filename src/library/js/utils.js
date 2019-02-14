55922776861ba32461fb5ba61ac92b11f68c4c88
Petrov Dmitry <dima.krutoo@yandex.ru>
2019-02-14 21:41:18 +0500

    new utils added

--- src/library/js/utils.js
+++ src/library/js/utils.js
@@ -1,6 +1,46 @@
 /**
- * Return a class name of input value (true way to get it).
- * @param {*} value Value to define it class.
+ * Functions class names.
+ * @type {Array<string>}
+ */
+const functionTags = Object.freeze([
+	'Function',
+	'AsyncFunction',
+	'GeneratorFunction',
+	'Proxy',
+]);
+
+/**
+ * Check that value is a function.
+ * @param {*} value Value.
+ * @return {boolean} Is it a function?
+ */
+export function isFunction (value) {
+	return functionTags.includes(classOf(value));
+}
+
+/**
+ * Check that value is boolean.
+ * @param {*} value Value.
+ * @return {boolean} Is it boolean?
+ */
+export function isBoolean (value) {
+	return classOf(value) === 'Boolean';
+}
+
+/**
+ * Check that value is primitive.
+ * @param {*} value Value to determine is it primitive.
+ * @return {boolean} Is it primitive?
+ */
+export function isPrimitive (value) {
+	return value !== Object(value);
+}
+
+/**
+ * Return a class of input value (true way to get it).
+ * @param  {*} value Value to define it class.
  * @return {string} Name of class.
  */
-export const classOf = value => Object.prototype.toString.call(value).slice(8, -1);
+export function classOf (value) {
+	return Object.prototype.toString.call(value).slice(8, -1);
+}
